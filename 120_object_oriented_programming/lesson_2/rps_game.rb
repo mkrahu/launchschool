3cb926071940fa83908d7a6349d2dfa1faab7f4d
Mike Krahulec <mike.krahulec@gmail.com>
2018-04-04 22:21:47 -0500

    Add smart computer selections to the RPS game in Object Oriented Programming course

--- 120_object_oriented_programming/lesson_2/rps_game.rb
+++ 120_object_oriented_programming/lesson_2/rps_game.rb
@@ -1,11 +1,35 @@
 #!/usr/bin/env ruby -wKU
 # rps_game.rb
 
+require 'pry'
+
+class History
+  attr_accessor :log, :results
+
+  def initialize
+    @log = []
+    create_results
+  end
+
+  def create_results
+    @results = Hash.new
+    Move::VALUES.each do |value|
+      results[value] = { 'win' => 0, 'tie' => 0, 'loss' => 0 }
+    end
+  end
+
+  def add_result(move, result)
+    @log << move.value
+    results[move.value][result] += 1
+  end
+end
+
 class Player
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :history
 
   def initialize
     set_name
+    @history = History.new
   end
 end
 
@@ -14,7 +38,7 @@ class Human < Player
     n = ''
     loop do
       print "What's your name? "
-      n = $stdin.gets.chomp
+      n = $stdin.gets.chomp.capitalize
       break unless n.empty?
       puts "Sorry, please provide your name..."
     end
@@ -24,7 +48,8 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      print "Please choose #{Move::VALUES[0..-2].join(', ')} or #{Move::VALUES[-1]}: "
+      print "Please choose " \
+            "#{Move::VALUES[0..-2].join(', ')} or #{Move::VALUES[-1]}: "
       choice = $stdin.gets.chomp
       break if Move::VALUES.include? choice
       puts "Sorry, invalid choice..."
@@ -39,28 +64,40 @@ class Computer < Player
   end
 
   def choose
-    self.move = Move.new(Move::VALUES.sample)
+    self.move = Move.new(weight_choices.sample)
+  end
+
+  def weight_choices
+    choices = []
+    Move::VALUES.each do |move|
+      move_results = history.results[move]
+      weight = 3 + (move_results['win'] - move_results['loss'])
+      choices += [move] * (weight < 0 ? 0 : weight)
+    end
+    choices
   end
 end
 
 class Move
-  VALUES = [ 'rock', 'paper', 'scissors', 'lizard', 'spock' ]
+  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
   WIN_HASH = {  'rock'     => ['scissors', 'lizard'],
                 'paper'    => ['rock', 'spock'],
                 'scissors' => ['paper', 'lizard'],
                 'lizard'   => ['spock', 'paper'],
                 'spock'    => ['scissors', 'rock'] }
 
+  attr_reader :value
+
   def initialize(value)
     @value = value
   end
 
   def >(other)
-    WIN_HASH[@value].include?(other)
+    WIN_HASH[@value].include?(other.value)
   end
 
   def <(other)
-    ! WIN_HASH[@value].include?(other)
+    WIN_HASH[other.value].include?(@value)
   end
 
   def to_s
@@ -71,12 +108,13 @@ end
 class RPSRound
   WINNING_SCORE = 3
 
-  attr_accessor :human, :computer, :score
+  attr_accessor :human, :computer, :score, :winner
 
   def initialize(human, computer)
     @human = human
     @computer = computer
     @score = { human.name => 0, computer.name => 0 }
+    @winner = nil
   end
 
   def display_moves
@@ -86,14 +124,17 @@ class RPSRound
   end
 
   def calculate_turn
+    winner = nil
     if human.move > computer.move
-      human.name
+      winner = human.name
     elsif human.move < computer.move
-      computer.name
+      winner = computer.name
     end
+    update_history(winner)
+    winner
   end
 
-  def display_winner
+  def display_results
     turn_winner = calculate_turn
     if turn_winner
       puts "#{turn_winner} won!"
@@ -103,16 +144,30 @@ class RPSRound
     end
   end
 
+  def update_history(winner)
+    case winner
+    when human.name
+      human.history.add_result(human.move, 'win')
+      computer.history.add_result(computer.move, 'loss')
+    when computer.name
+      human.history.add_result(human.move, 'loss')
+      computer.history.add_result(computer.move, 'win')
+    else
+      human.history.add_result(human.move, 'tie')
+      computer.history.add_result(computer.move, 'tie')
+    end
+  end
+
   def display_score
     puts "Score is now #{human.name}: #{score[human.name]}, " \
       "#{computer.name}: #{score[computer.name]}"
   end
 
-  def winner?
+  def round_winner?
     score.value?(WINNING_SCORE)
   end
 
-  def winner
+  def round_winner
     score.key(WINNING_SCORE)
   end
 
@@ -122,11 +177,11 @@ class RPSRound
       human.choose
       computer.choose
       display_moves
-      display_winner
+      display_results
       display_score
-      break if winner?
+      break if round_winner?
     end
-    winner
+    self.winner = round_winner
   end
 end
 
@@ -139,11 +194,14 @@ class RPSGame
   end
 
   def display_welcome_message
-    puts "Welcome to #{Move::VALUES[0..-2].join(', ')} or #{Move::VALUES[-1]} #{human.name}!"
+    system 'clear'
+    puts "Welcome to " \
+         "#{Move::VALUES[0..-1].map(&:capitalize).join(', ')} #{human.name}!"
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock... Goodbye!"
+    puts "Thanks for playing " \
+         "#{Move::VALUES[0..-1].map(&:capitalize).join(', ')}... Goodbye!"
   end
 
   def play_again?
@@ -162,8 +220,8 @@ class RPSGame
     display_welcome_message
     loop do
       round = RPSRound.new(human, computer)
-      winner = round.play
-      puts "#{winner} won the game!"
+      round.play
+      puts "#{round.winner} won the game!"
       break unless play_again?
     end
     display_goodbye_message
