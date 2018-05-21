cd11328f06bb6439191631905f651c85ae94c09a
Mike Krahulec <mike.krahulec@gmail.com>
2018-05-21 08:44:17 -0500

    Create new folder for second attempt at oop exercises medium 1

---
+++ exercises/object_oriented_programming/vers_2/medium_1/privacy.rb
@@ -0,0 +1,22 @@
+# privacy.rb
+
+class Machine
+  def start
+    flip_switch(:on)
+  end
+
+  def stop
+    flip_switch(:off)
+  end
+
+  private
+
+  attr_writer :switch
+
+  def flip_switch(desired_state)
+    self.switch = desired_state
+  end
+end
+
+machine = Machine.new
+machine.start
---
+++ exercises/object_oriented_programming/vers_2/medium_1/stack_machine_interpretation.rb
@@ -0,0 +1,13 @@
+# stack_machine_interpretation.rb
+
+class Minilang
+  def initialize(commands)
+    @commands = commands.split
+    @register = 0
+    @stack = []
+  end
+
+  def eval
+    
+  end
+end
