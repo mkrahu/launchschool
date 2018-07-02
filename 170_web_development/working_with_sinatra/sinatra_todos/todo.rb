d319558be07ec5fe271317cba0d904667601ba9f
Mike Krahulec <mike.krahulec@gmail.com>
2018-07-02 16:18:50 -0500

    Complete first half of sinatra todos and downloaded the completed app for reference

---
+++ 170_web_development/working_with_sinatra/sinatra_todos/todo.rb
@@ -0,0 +1,46 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+def list_name_error(name)
+  if session[:lists].any? { |list| list[:name] == name}
+    'List name must be unique'
+  elsif !(1..100).cover? name.size
+    'List name must be 1-100 characters'
+  end
+end
+
+post "/lists" do
+  list_name = params['list_name'].strip
+  
+  if error = list_name_error(list_name)
+    session[:error] = error
+    redirect "/lists/new"
+  else
+    session[:lists] << { name: params['list_name'], todos: [] }
+    session[:success] = 'You have created a new list.'
+    redirect '/lists'
+  end
+end
