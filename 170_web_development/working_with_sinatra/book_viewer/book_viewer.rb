f235694f934c4c1698fa5e3b4bd9dba63927c429
Mike Krahulec <mike.krahulec@gmail.com>
2018-11-17 16:05:39 -0600

    Create before filter/helpers and redirect for non-valid pages

--- 170_web_development/working_with_sinatra/book_viewer/book_viewer.rb
+++ 170_web_development/working_with_sinatra/book_viewer/book_viewer.rb
@@ -1,20 +1,35 @@
 require "sinatra"
 require "sinatra/reloader"
 
+helpers do
+  def in_paragraphs(text)
+    paragraphs = text.split("\n\n")
+    paragraphs.map { |paragraph| "<p>#{paragraph}</p>" }.join
+  end
+end
+
+before do
+  @contents = File.readlines('data/toc.txt')
+end
+
 get "/" do
   @title = 'The Adventures of Sherlock Holmes'
-  @contents = File.readlines('data/toc.txt')
 
   erb :home
 end
 
 get "/chapter/:number" do
-  @contents = File.readlines('data/toc.txt')
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
 
-  number = params[:number]
-  @title = "Chapter #{number}"
+  redirect "/" unless (1..@contents.size).cover? number
 
-  @chapter = File.readlines("data/chp#{number}.txt","\n\n")
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+not_found do
+  redirect "/"
 end
