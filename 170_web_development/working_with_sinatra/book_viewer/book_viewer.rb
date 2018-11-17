d80009a2ace7d1883b68a992ab142bf4c91e0300
Mike Krahulec <mike.krahulec@gmail.com>
2018-11-17 14:24:27 -0600

    Complete first part of book_viewer app and restart dynamic directory coding challenge

--- 170_web_development/working_with_sinatra/book_viewer/book_viewer.rb
+++ 170_web_development/working_with_sinatra/book_viewer/book_viewer.rb
@@ -2,5 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  @title = 'The Adventures of Sherlock Holmes'
+  @contents = File.readlines('data/toc.txt')
+
+  erb :home
+end
+
+get "/chapter/:number" do
+  @number = params[:number]
+  @title = "Chapter #{@number}"
+  @contents = File.readlines('data/toc.txt')
+  @chapter = File.readlines("data/chp#{@number}.txt","\n\n")
+
+  erb :chapter
 end
