9bad9f07997cc10261e14b953f0cac4a4af05918
Mike Krahulec <mike.krahulec@gmail.com>
2018-11-18 22:39:03 -0600

    Complete the users and interests code challenge

--- 170_web_development/working_with_sinatra/users_and_interests/users_and_interests.rb
+++ 170_web_development/working_with_sinatra/users_and_interests/users_and_interests.rb
@@ -6,12 +6,27 @@ before do
   @users = YAML.load_file('data/users.yaml')
 end
 
+helpers do
+  def count_interests
+    count = 0
+
+    @users.each do |name, info|
+      count += info[:interests].size
+    end
+
+    count
+  end
+end
+
 get "/" do
+  @title = 'Users'
+
   erb :users
 end
 
 get "/user/:name" do
   @name = params[:name]
+  @title = @name
   @interests = @users[@name.to_sym]
 
 
