f9a7092c4d5fe233f9ad81de8ea490150525f655
Mike Krahulec <mike.krahulec@gmail.com>
2018-11-27 10:46:47 -0600

    Cleanup todo.rb with rubocop notes

--- 170_web_development/todos/todo.rb
+++ 170_web_development/todos/todo.rb
@@ -1,6 +1,6 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -11,34 +11,33 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
 # View all of the lists
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
 
   erb :lists, layout: :layout
 end
 
 # Render the new list form
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list
-
 end
 
 # Return error message if name is invalid. Return nil if name is valid.
 def error_in_list_name(name)
   if !(1..100).cover?(name.size)
     'List name must be between 1 and 100 characters.'
-  elsif session[:lists].any? { |list| list[:name] == name}
+  elsif session[:lists].any? { |list| list[:name] == name }
     'List name must be unique'
   end
 end
 
 # Create a new list
-post "/lists" do
+post '/lists' do
   list_name = params[:list_name].strip
 
   error = error_in_list_name(list_name)
@@ -47,8 +46,8 @@ post "/lists" do
     erb :new_list, layout: :layout
   else
     session[:lists] << { name: list_name, todos: [] }
-    session[:success] = "The list has been created."
+    session[:success] = 'The list has been created.'
 
-    redirect "/lists"
+    redirect '/lists'
   end
 end
