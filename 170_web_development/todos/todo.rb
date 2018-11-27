5fb09866d84e2e139481c9470883c29bdb7cbe63
Mike Krahulec <mike.krahulec@gmail.com>
2018-11-27 10:38:01 -0600

    Refactor the validation of list names

--- 170_web_development/todos/todo.rb
+++ 170_web_development/todos/todo.rb
@@ -28,18 +28,27 @@ get "/lists/new" do
 
 end
 
+# Return error message if name is invalid. Return nil if name is valid.
+def error_in_list_name(name)
+  if !(1..100).cover?(name.size)
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name}
+    'List name must be unique'
+  end
+end
+
 # Create a new list
 post "/lists" do
-  name = params[:list_name].strip
+  list_name = params[:list_name].strip
 
-  if (1..100).cover?(name.size)
-    session[:lists] << { name: name, todos: [] }
+  error = error_in_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created."
 
     redirect "/lists"
-  else
-    session[:error] = 'List name must be between 1 and 100 characters.'
-
-    erb :new_list
   end
 end
